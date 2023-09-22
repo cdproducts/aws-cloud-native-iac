11299a7379609c7fd4225559d3867f4815f013c6
hrishikeshkakkad <hrishidkakkad@gmail.com>
2023-09-21 23:25:08 -0400

    Revampd code 4

--- src/stacks/Applications.stack.ts
+++ src/stacks/Applications.stack.ts
@@ -1,16 +1,7 @@
-import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import * as ecs from "aws-cdk-lib/aws-ecs";
-import * as iam from "aws-cdk-lib/aws-iam";
 import { Microservice } from "../constructs/ecsCompute";
-import * as ec2 from "aws-cdk-lib/aws-ec2";
-import * as elb from "aws-cdk-lib/aws-elasticloadbalancingv2";
-import * as servicediscovery from "aws-cdk-lib/aws-servicediscovery";
-import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
-import { AwsSecurityGroups } from "../constructs/security";
 import { ImageRepositoryStack } from "./ImageRepository.stack";
-import { CodeRepositoryStack } from "./CodeRepository.stack";
-import { config } from "dotenv";
 import { NetworkingStack } from "./Networking.stack";
 import { ClusterStack } from "./EcsCluster.stack";
 import { tfPimMicroservice } from "./services/tfPim";
